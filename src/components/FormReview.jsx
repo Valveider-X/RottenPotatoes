aa372c159269f836e480f63eebf07f108ebef594
Ronnie Vélez <ronnievelz@gmail.com>
2024-05-14 17:13:38 +0200

    review-rating-potatoe-form

--- src/components/FormReview.jsx
+++ src/components/FormReview.jsx
@@ -3,17 +3,42 @@ import React, { useState } from "react";
 function FormReview() {
 
   const [nameValue, setNameValue]=useState("")
+  const [commentValue, setCommentValue]=useState("")
+  const [topping, setTopping]=useState("")
+
 
   const handleNameChange=(e)=>{
    // console.log("escribiendo", e.target.value);
-    setNameValue(e.target.value.toUpperCase().replace("ALFONSO", "ALFONSO MANUEL"))
+    setNameValue(e.target.value.toUpperCase().replace("BULBASAUR", "CHARMANDER MANUEL"))
+  }
+  const handleCommentChange=(e)=>{
+
+    setCommentValue (e.target.value.toUpperCase())
+  }
+  const handleTopping=(e)=>{
+   // console.log("point");
+    setTopping(e.target.value)
   }
 
+  const handleSubmit=(e)=>{
+    e.preventDefault()
+    console.log("entregando");
+
+    const newComment={
+
+      name: nameValue,
+      comment: commentValue,
+      ratings:topping,
+
+    }
+    console.log("newComment");
+
+  }
   return (
     <div>
       FormReview
       <h2>Review</h2>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>
           <label>Name:</label>
           <input type="text" name="name" value={nameValue} onChange={handleNameChange}/>
@@ -21,7 +46,30 @@ function FormReview() {
 
         <div>
           <label>Comments:</label>
-          <input type="text" name="comments" />
+          <textarea type="text" name="comments" value={commentValue} onChange={handleCommentChange} />
+        </div>
+
+        <div>
+           
+          <label>Potatoes Rating:</label>
+          <br/>
+          <input type="radio" name="1"onChange={handleTopping}></input>
+          <label htmlFor="1">1</label>
+          <br/>
+          
+          <input type="radio" name="1"onChange={handleTopping}></input>
+          <label htmlFor="1">2</label>
+          <br/>
+          <input type="radio" name="1"onChange={handleTopping}></input>
+          <label htmlFor="1">3</label>
+          <br/>
+          <input type="radio" name="1"onChange={handleTopping}></input>
+          <label htmlFor="1">4</label>
+          <br/>
+          <input type="radio" name="1"onChange={handleTopping}></input>
+          <label htmlFor="1">5</label>
+          <br/>
+
         </div>
 
         <button>Are you sure?</button>
