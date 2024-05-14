31aeb767eff978e2087f6e51780dbd6bb67b2ec1
Alfonso <alfonsomanuel2000@gmail.com>
2024-05-14 17:14:02 +0200

    enlaces arreglados

--- src/App.jsx
+++ src/App.jsx
@@ -63,8 +63,8 @@ function App() {
         <Route path="/genres/:id" element={<GenresGames />} />
         <Route path="/games" element={<GamesList />} />
         <Route path="/game/:id" element={<GameDetails />} />
-        <Route path="/game-reviews/" element={<GameReviews />} />
-        <Route path="/game-completion/" element={<GameCompletion />} />
+        <Route path="/game-reviews/:id" element={<GameReviews />} />
+        <Route path="/game-completion/:id" element={<GameCompletion />} />
         <Route path="/error" element={<Error />} />
         <Route path="*" element={<NotFound />} />
         <Route path="/about" element={<About />} />
--- src/components/FormCompletion.jsx
+++ src/components/FormCompletion.jsx
@@ -3,15 +3,32 @@ import {useState} from "react"
 
 function FormCompletion() {
 
+  
+
+   <div className=""></div>
+
   const [ nameValue, setNameValue ] = useState("")
   return (
 
     <div className='completion-form'>
 
-    <div>FormCompletion</div>
     
+    <form>
+      <label >Completion Time:</label><br />
+      <input type="radio" id="<10" name="completionTime" value="<10" />
+      <label for="<10"> Menos de 10 horas</label><br></br>
+      <input type="radio" id="10><30" name="completionTime" value="10><30" />
+      <label for="10><30"> Entre 10 y 30 horas</label><br></br>
+      <input type="radio" id="30><50" name="completionTime" value="30><50" />
+      <label for="30><50"> Entre 30 y 50 horas</label><br></br>
+      <input type="radio" id=">50" name="completionTime" value=">50" />
+      <label for=">50"> Mas de 50 horas</label><br></br>
+      
+    
+
+    </form>
     </div>
-    
+
   )
 }
 
