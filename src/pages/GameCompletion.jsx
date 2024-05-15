a4cfdbcecff0435d0e121184fe704edd6c6f1ddd
Alfonso <alfonsomanuel2000@gmail.com>
2024-05-15 10:08:29 +0200

    en ello

--- src/pages/GameCompletion.jsx
+++ src/pages/GameCompletion.jsx
@@ -1,6 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 import FormCompletion from "../components/FormCompletion"
+import axios from 'axios'
+
 function GameCompletion() {
+
+  const params = useParams()
+  console.log(params);
+  const [completion, setCompletion] = useState(null)
+  
+  useEffect(()=>{
+    getData()
+
+  },[])
+
+const getData=()=>{
+  axios.get(`${import.meta.env.VITE_API_BACKEND}/completions`)
+
+  .then((response)=>{
+    console.log(response);
+    
+    
+  })
+  .catch((error)=>{
+    console.log(error)
+  })
+}
+
+
   return (
     <div>GameCompletion
       <FormCompletion/>
